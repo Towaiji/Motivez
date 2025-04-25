583adc16cd2c601081b6f4003af841852e15e451
Towaiji <aliyasiraltowaiji@gmail.com>
2025-04-25 04:22:20 -0400

    made tabs for navigation bar

---
+++ MotivezApp/app/(tabs)/_layout.tsx
@@ -0,0 +1,47 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: true,
+        tabBarActiveTintColor: "#e91e63",
+        tabBarInactiveTintColor: "#000",
+        tabBarStyle: {
+          backgroundColor: "#fff7fe",
+          borderTopWidth: 0,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          paddingBottom: 5,
+        },
+        tabBarItemStyle: {
+          padding: 10,
+          marginBottom: 5,
+        },
+        headerStyle: {
+          backgroundColor: "#fff7fe",
+        },
+      }}>
+      <Tabs.Screen name="index" options={{
+        headerTitle: "Motivez",
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
+      }} />
+      <Tabs.Screen name="user" options={{
+        headerTitle: "My Profile",
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color }) => <Ionicons name="person" size={24} color={color} />,
+      }} />
+      <Tabs.Screen name="motives" options={{
+        headerTitle: "Find Motivez",
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color }) => <Ionicons name="search" size={24} color={color} />,
+      }} />
+      <Tabs.Screen name="+not-found" options={{
+        headerTitle: "Not Found",
+      }} />
+    </Tabs>
+  );
+}
