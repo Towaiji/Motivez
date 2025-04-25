588701168b1f991447622465846c18f493b6db9c
Towaiji <aliyasiraltowaiji@gmail.com>
2025-04-25 04:22:56 -0400

    layout for tabs

--- MotivezApp/app/_layout.tsx
+++ MotivezApp/app/_layout.tsx
@@ -1,9 +1,20 @@
 import { Stack } from "expo-router";
+import { LogBox, StatusBar } from "react-native";
+
+LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
+    <>
+    <StatusBar barStyle="dark-content" />
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" options={{
+        headerShown: false,
+      }} />
+      <Stack.Screen name="+not-found" options={{
+        headerTitle: "Not Found",
+      }} />
     </Stack>
-    );
+    </>
+  );
 }
