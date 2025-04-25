44e01ca60896b8ea1d53af649a6d4b5df5a26e02
Towaiji <aliyasiraltowaiji@gmail.com>
2025-04-25 04:23:12 -0400

    not found screen incase of error

---
+++ MotivezApp/app/+not-found.tsx
@@ -0,0 +1,38 @@
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Link, Stack } from "expo-router";
+
+export default function NotFoundScreen() {
+    return (
+        <>
+            <Stack.Screen options={{ title: "404 - Not Found" }} />
+            <View style={styles.container}>
+                <Link href="/" style={styles.button}>
+                    Go back to Home screen!
+                </Link>
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#efe7ee",
+    },
+    text: {
+        fontSize: 30,
+        color: "black",
+    },
+    button: {
+        backgroundColor: "#4CAF50",
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 20,
+    },
+    buttonText: {
+        color: "black",
+        fontWeight: "bold",
+    },
+});
