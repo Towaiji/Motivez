b6877d081e6a3e950dc88c8ae371b4a32c31dbc7
Mohammad Alkukhun <akukhun2005@gmail.com>
2025-06-20 23:11:06 -0400

    ability to show what motives you made on the my motivez tab

--- MotivezApp/app/_layout.tsx
+++ MotivezApp/app/_layout.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Stack } from "expo-router";
 import { LogBox, StatusBar } from "react-native";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ScrollProvider } from "./context/ScrollContext";
+import { supabase } from '../lib/supabaseClient';
 
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    const login = async () => {
+      const { error } = await supabase.auth.signInWithPassword({
+        email: "test@motivez.com",
+        password: "test123",
+      });
+      if (error) console.error("Login failed", error);
+      else console.log("✅ Test user logged in");
+    };
+    login();
+  }, []);
+  
+
   return (
     <SafeAreaProvider>
       <StatusBar barStyle="dark-content" />
